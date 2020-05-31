8a48dd6c518c03778062c0931c6375d17c8be316
hamidreza <hrfarzadeh@gmail.com>
2020-05-31 02:29:19 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import './App.css';
+import './grid.css';
+import countriesAll from'./countriesAll.json'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+      <div className="row">
+    
+      {countriesAll.map((obj,index)=>{
+        return(
+          <div key={index} className="card lg-col-2">
+      
+            <img src={obj.flag}/>
+            
+            <ul>
+            <li className="country-name">{obj.name}</li>
+            <li>population: {obj.population}</li>
+            <li>Region: {obj.region}</li>
+            <li>Capital: {obj.capital}</li>
+            </ul>
+          </div>
+        )
+      })}
+      
+      </div>
      
     </div>
   );
